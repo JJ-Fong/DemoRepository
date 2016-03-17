3a4717d1dabb8fb30b78444843957fe981437562
Javier Fong <Javier Fong@172.20.5.101>
2016-03-17 10:49:43 -0600

    Version  10:45 17/3/2016

--- src/github/demo/GitHubDemo.java
+++ src/github/demo/GitHubDemo.java
@@ -16,7 +16,7 @@ public class GitHubDemo {
      */
     public static void main(String[] args) {
         // TODO code application logic here
-        System.out.println("Branch");
+        System.out.println("Branch 2.0s");
     }
     
 }
